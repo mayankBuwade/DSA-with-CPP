bad36dfe9ae80e0dfb2d567e7244626c8ca7f5ae
mayank_buwade <mayank.buade@gmail.com>
2023-04-09 01:35:33 +0530

    circular linked list class based approach

--- 06LinkedList/03.cpp
+++ 06LinkedList/03.cpp
@@ -1,4 +1,5 @@
 //circular linked list
+
 #include<iostream>
 
 using namespace std;
@@ -19,8 +20,30 @@ public:
     }
 };
 
-Node* insertAtHead(Node *head, int val)
+class CLL {
+private:
+    Node *head;
+public:
+    CLL();
+    void insertAtHead(int);
+    void insertAtTail(int);
+    void insertAtIndex(int, int);
+    void deleteAtIndex(int);
+    int get(int);
+    void print();
+};
+
+CLL::CLL() {
+    head = NULL;
+}
+
+void CLL::insertAtHead(int val)
 {
+    if(!head) 
+    {
+        head = new Node(val);
+        return;
+    }
     Node *newHead = new Node(val, head);
     Node *temp = head;
     //iterating to the last node so can update its next pointer
@@ -29,13 +52,20 @@ Node* insertAtHead(Node *head, int val)
         temp = temp->next;
     }
     temp->next = newHead;
-    return newHead;
+    //updating current head
+    head = newHead;
 }
 
-void insertAtTail(Node *head, int val)
+void CLL::insertAtTail(int val) 
 {
+    if(head == NULL) 
+    {
+        insertAtHead(val);
+        return;
+    }
     Node *tail = new Node(val, head);
     Node *temp = head;
+    //iterating to the tail
     while(temp->next != head)
     {
         temp = temp->next;
@@ -43,15 +73,16 @@ void insertAtTail(Node *head, int val)
     temp->next = tail;
 }
 
-Node* insertAtIndex(Node *head, int val, int index)
+void CLL::insertAtIndex(int index, int val)
 {
     if(head == NULL)
     {
-        return NULL;
+        return;
     }
     if(index == 0)
     {
-        return insertAtHead(head, val);
+        insertAtHead(val);
+        return;
     }
     Node *temp = head;
     while(temp->next != head && --index)
@@ -61,60 +92,39 @@ Node* insertAtIndex(Node *head, int val, int index)
     if(index != 0 && index != 1) 
     {
         cout<<"out of range"<<endl;
-        return head;
+        return;
     }
     temp->next = new Node(val, temp->next);
-    return head;
 }
 
-Node* deleteHead(Node* head)
-{
-    if(head == NULL) return head;
-    if(head->next == head) 
-    {
-        delete head;
-        return NULL;
-    }
-    Node* newHead = head->next;
-    Node* temp = head;
-    while(temp->next != head)
-    {
-        temp = temp->next;
-    }
-    temp->next = newHead;
-    delete head;
-    return newHead;
-}
-
-Node* deleteTail(Node *head)
-{
-    if(head == NULL) return head;
-    if(head->next == head) 
-    {
-        delete head;
-        return NULL;
-    }
-    Node* temp = head;
-    while(temp->next->next != head)
-    {
-        temp = temp->next;
-    }
-    Node* deletingNode = temp->next;
-    temp->next = deletingNode->next;
-    delete deletingNode;
-    return head;
-}
-
-Node* deleteAtIndex(Node *head, int index)
+void CLL::deleteAtIndex(int index)
 {
     if(head == NULL)
     {
-        return NULL;
+        return;
     }
+    //deleting head
     if(index == 0)
     {
-        return deleteHead(head);
+        if(head == NULL) return;
+        if(head->next == head) 
+        {
+            delete head;
+            head = NULL;
+        }
+        Node* newHead = head->next;
+        Node* temp = head;
+        while(temp->next != head)
+        {
+            temp = temp->next;
+        }
+        //updating next of the last node
+        temp->next = newHead;
+        delete head;
+        head = newHead;
+        return;
     }
+    //delete at any indext other than head
     Node *temp = head;
     while(temp->next != head && --index)
     {
@@ -123,18 +133,39 @@ Node* deleteAtIndex(Node *head, int index)
     if(index != 0) 
     {
         cout<<"out of range"<<endl;
-        return head;
+        return;
     }
     Node* deletingNode = temp->next;
     temp->next = deletingNode->next;
     delete deletingNode;
-    return head;
+    return;
 }
 
-void printCLL(Node *head)
+int CLL::get(int index)
 {
-    Node* temp = head;
-    if(head == NULL) return;
+    Node *temp = head;
+    if(head == NULL)
+    {
+        cout<<"List is empty"<<endl;
+        return -1;
+    }
+    while(index && temp->next != head)
+    {
+        temp = temp->next;
+        --index;
+    }
+    if(index == 0)
+    {
+        return temp->val;
+    }
+    cout<<"out of range"<<endl;
+    return -1;
+}
+
+void CLL::print()
+{
+    if(!head) return;
+    Node *temp = head;
     cout<<temp->val<<" ";
     while(temp->next != head)
     {
@@ -146,16 +177,16 @@ void printCLL(Node *head)
 
 int main()
 {
-    //system("cls");
-    Node *head;
-    head = new Node(5);
-    head = insertAtHead(head, 10);
-    head = insertAtHead(head, 15);
-    head = insertAtHead(head, 20);
-    head = insertAtHead(head, 25);
-    printCLL(head);
-    //head = deleteHead(head);
-    head = deleteAtIndex(head, 5);
-    printCLL(head);
+    system("cls");
+    CLL l1;
+    l1.insertAtTail(1);
+    l1.insertAtTail(2);
+    l1.insertAtTail(3);
+    l1.insertAtTail(4);
+    l1.insertAtHead(99);
+    l1.insertAtIndex(0, 55);
+    l1.deleteAtIndex(2);
+    l1.print();
+    cout<<l1.get(-1);
     return 0;
 }
